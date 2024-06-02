1dc29339f1ed8511fb1fa919841a11b5c450e9b4
Grey Tie <ultrafutile@protonmail.com>
2024-06-02 18:22:44 -0500

    Extract Timer custom element

---
+++ 1-basics/src/lib/Timer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let counter: number = 30;
+	let currentTimeoutID: number;
+
+	async function startCounterHandler() {
+		counter = 30;
+		for (let i = 0; i < 30; i++) {
+			await new Promise((f) => currentTimeoutID = setTimeout(f, 1000));
+			console.log(currentTimeoutID);
+			counter--;
+			console.log(counter);
+		}
+	}
+
+	function cancelCounter() {
+		clearTimeout(currentTimeoutID);
+		counter = 30;
+	}
+</script>
+
+<div class="counter-element">
+	<div>Counter: {counter}</div>
+	<button on:click={startCounterHandler}>Start counter</button>
+	<button on:click={cancelCounter}>Cancel counter</button>
+</div>
+
+<style>
+	.counter-element {
+		display: flex;
+		flex-direction: column;
+		/* justify-content: center; */
+	}
+</style>
--- 1-basics/src/routes/+page.svelte
+++ 1-basics/src/routes/+page.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
-	let counter: number = 30;
-	let currentTimeoutID: number;
-
-	async function startCounterHandler() {
-		counter = 30;
-		for (let i = 0; i < 30; i++) {
-			await new Promise((f) => currentTimeoutID = setTimeout(f, 1000));
-			console.log(currentTimeoutID);
-			counter--;
-			console.log(counter);
-		}
-	}
-
-	function cancelCounter() {
-		clearTimeout(currentTimeoutID);
-		counter = 30;
-	}
+	import Timer from '../lib/Timer.svelte'
 </script>
 
 <h1>1-Basics</h1>
 <div class="counter-container">
-	<div>Counter: {counter}</div>
-	<button on:click={startCounterHandler}>Start counter</button>
-	<button on:click={cancelCounter}>Cancel counter</button>
+	<Timer></Timer>
 </div>
-
-
 
 <style>
 	.counter-container {
