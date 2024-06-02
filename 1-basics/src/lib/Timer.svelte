20b1f3179de72b40c9443ecd9a1c5dd4d2c0741d
Grey Tie <ultrafutile@protonmail.com>
2024-06-02 18:35:34 -0500

    Some styling

--- 1-basics/src/lib/Timer.svelte
+++ 1-basics/src/lib/Timer.svelte
@@ -19,15 +19,39 @@
 </script>
 
 <div class="counter-element">
-	<div>Counter: {counter}</div>
-	<button on:click={startCounterHandler}>Start counter</button>
-	<button on:click={cancelCounter}>Cancel counter</button>
+	<div class="counter-display">Counter: {counter}</div>
+	<div class="button-container">
+		<button on:click={startCounterHandler}>Start counter</button>
+		<button on:click={cancelCounter}>Cancel counter</button>
+	</div>	
 </div>
 
 <style>
 	.counter-element {
 		display: flex;
 		flex-direction: column;
-		/* justify-content: center; */
+		font-size: 8em;
+	}
+
+	.counter-display {
+		text-transform: uppercase;
+	}
+
+	.button-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		gap: 20px;
+	}
+
+	button {
+		padding: 10px;
+		background-color: white;
+		border-radius: 12px;
+	}
+
+	button:hover {
+		background-color: lightgray;
+		cursor: pointer;
 	}
 </style>
