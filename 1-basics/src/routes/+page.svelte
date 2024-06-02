6cb1f5a07f91789759c7d0977525285832e325e5
Grey Tie <ultrafutile@protonmail.com>
2024-06-02 17:58:23 -0500

    Initial commit

---
+++ 1-basics/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	let counter = 0;
+
+	async function startCounterHandler() {
+		counter = 0;
+		for (let i = 0; i < 30; i++) {
+			await new Promise((f) => setTimeout(f, 1000));
+			counter++;
+			console.log(counter);
+		}
+	}
+</script>
+
+<h1>1-Basics</h1>
+<div class="counter-container">
+	<div>Counter: {counter}</div>
+	<button on:click={startCounterHandler}>Start counter</button>
+</div>
+
+
+
+<style>
+	.counter-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+</style>
