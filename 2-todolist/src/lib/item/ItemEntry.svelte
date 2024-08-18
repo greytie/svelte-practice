784ac4436f73376dd9d7eceef365d4b5016e1245
Grey Tie <ultrafutile@protonmail.com>
2024-08-17 19:28:33 -0500

    Add checkbox click handling

--- 2-todolist/src/lib/item/ItemEntry.svelte
+++ 2-todolist/src/lib/item/ItemEntry.svelte
@@ -11,10 +11,24 @@
 			itemID: itemID
 		});
 	}
+
+	function checkboxClicked(event: Event) {
+		console.log((<HTMLInputElement>event.target).checked);
+		item.completed = (<HTMLInputElement>event.target).checked;
+		// TODO: DB Update should happen
+	}
+
 </script>
 
 <div>
-	<input type="checkbox"/>
-	<label for="An item">{item.name}</label>
+	<input bind:value={item.completed} on:change={(event) => checkboxClicked(event)} type="checkbox"/>
+	<label for="An item" class={item.completed ? 'completed' : ''}>{item.name}</label>
 	<button on:click={() => dispatchDeleteItem(item.id)}>Delete</button>
 </div>
+
+
+<style>
+	.completed {
+		text-decoration: line-through;
+	}
+</style>
